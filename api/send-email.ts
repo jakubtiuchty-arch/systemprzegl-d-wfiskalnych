78a802ca5d4b7a4368905d35135852db423e2567
Jakub Tiuchty <jakubtiuchty@Macbook-M3.local>
2025-11-22 17:04:13 +0100

    Fix: use kontakt@rejestratory.info as sender

--- api/send-email.ts
+++ api/send-email.ts
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
 
     try {
         const { data, error } = await resend.emails.send({
-            from: 'System przeglądów fiskalnych <przeglądy@rejestratory.info>',
+            from: 'System przeglądów fiskalnych <kontakt@rejestratory.info>',
             to,
             subject,
             html,
